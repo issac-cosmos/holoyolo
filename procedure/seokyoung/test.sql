a8f841b8632d3be3420317298a17e9608f8c93fc
ghdtjrdud <ghdtjrdud38@gmail>
2024-12-02 18:12:32 +0900

    merge 충돌해결

--- procedure/seokyoung/test.sql
+++ procedure/seokyoung/test.sql
@@ -38,13 +38,27 @@ END
 -------- 회원정보 조회
 
 DELIMITER //
-create procedure 회원정보조회(in inputTitle varchar(255), in inputContents varchar(255), in inputEmail varchar(255)
+CREATE procedure 회원정보조회(
+in inputName varchar(255)
+)
+
 begin
-    declare authorId bigint;
-    declare postId bigint;
-    insert into post(title, contents) valuse(inputTitle, inputContents);
-    select id into postId from post order by id desc limit 1;
-    select id into authorId from author where email = inputEmail;
-    insert into author_post(author_id, post_id) values(authorId,postId);
+
+    select * from consumer where name = inputName;
+end
+//DELIMITER ;
+
+-------- 회원정보 수정
+
+DELIMITER //
+CREATE procedure 회원정보수정(
+in inputId bigint,
+in inputPhone varchar(255),
+in inputPass varchar(255)
+)
+
+begin
+
+    update consumer set phone_number = inputPhone, password = inputPass where consumer_id = inputId;
 end
 //DELIMITER ;
