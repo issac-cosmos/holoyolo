0f90dd0341678c244a32cccd0078c1db2cf53063
ghdtjrdud <ghdtjrdud38@gmail>
2024-12-02 19:36:37 +0900

    프로시저 테스트 중간커밋

--- procedure/seokyoung/test.sql
+++ procedure/seokyoung/test.sql
@@ -58,7 +58,77 @@ in inputPass varchar(255)
 )
 
 begin
+    
+    update consumer set phone_number = inputPhone, password = inputPass 
+    where consumer_id = inputId;
 
-    update consumer set phone_number = inputPhone, password = inputPass where consumer_id = inputId;
+end
+//DELIMITER ;
+
+-------- 회원탈퇴
+
+DELIMITER //
+CREATE procedure 회원탈퇴(
+in inputId bigint
+)
+
+begin
+    DECLARE exit HANDLER FOR SQLEXCEPTION
+    BEGIN
+        -- 예외 발생 시 실패 메시지 출력
+        SELECT '탈퇴실패' AS message;
+    END;
+
+    update consumer set quit = "Y" where consumer_id = inputId;
+
+    SELECT '탈퇴성공' AS message;
+end
+//DELIMITER ;
+
+-------- 상품등록
+
+DELIMITER //
+CREATE procedure 상품등록(
+in input_Pname varchar(255),
+in input_price bigint,
+in input_Sid bigint,
+in input_Aid bigint,
+in input_Pcon varchar(3000),
+in input_Pic varchar(3000)
+)
+
+begin
+    DECLARE exit HANDLER FOR SQLEXCEPTION
+    BEGIN
+        -- 예외 발생 시 실패 메시지 출력
+        SELECT '등록실패' AS message;
+    END;
+
+    insert into product(product_name, price, seller_id,approve_admin_id, product_contents, picture)
+    values(input_Pname, input_price, input_Sid, input_Aid, input_Pcon, input_Pic);
+
+    SELECT '등록성공' AS message;
+end
+//DELIMITER ;
+
+-------- 상품승인
+
+DELIMITER //
+CREATE procedure 상품승인(
+in input_Pid bigint,
+in input_Aid bigint
+)
+
+begin
+    DECLARE exit HANDLER FOR SQLEXCEPTION
+    BEGIN
+        -- 예외 발생 시 실패 메시지 출력
+        SELECT '승인실패' AS message;
+    END;
+
+    update product set approve = 'y', approve_admin_id = input_Aid 
+    where product_id = input_Pid;
+
+    SELECT '승인성공' AS message;
 end
 //DELIMITER ;
